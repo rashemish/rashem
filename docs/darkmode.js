eca1b75200b084a7bb640cb5be5f1c0dceaf58b6
rashemish <rvm5988@psu.edu>
2025-10-03 15:28:20 -0400

    updating portfolio

--- docs/darkmode.js
+++ docs/darkmode.js
@@ -2,21 +2,28 @@
 
 //toggle button: my website logo
 document.addEventListener("DOMContentLoaded", () => {
-  const logo = document.querySelector(".logo");
-  const body = document.body;
-  const helloText = document.querySelector(".hello-text, #hello-text"); 
+const logo = document.querySelector(".logo");
+const body = document.body;
+const helloText = document.querySelector(".hello-text, #hello-text");
 
-//toggle function: changes header,body, and hello-text to darkmode
-  if (logo) {
-    logo.style.cursor = "pointer";
+//toggle function: changes body, hello-text, and hello-image to darkmode
+if (logo) {
+logo.style.cursor = "pointer"; //this shows that the logo is clickable
 
-    logo.addEventListener("click", () => {
-      body.classList.toggle("dark-mode");
+logo.addEventListener("click", () => { //click!
+body.classList.toggle("dark-mode"); //toggles dark mode
 
-      if (helloText) {
-        helloText.classList.toggle("dark-hello");
-      }
-    });
-  }
+if (helloText) {
+helloText.classList.toggle("dark-hello"); //toggles darkmode text
+}
+if (helloImg) {
+if (helloImg.src.includes("rashewave2.png")) { //toggles darkmode image
+helloImg.src = "images/rashewave3.png";
+} else {
+helloImg.src = "images/rashewave2.png";
+}
+}
+});
+}
 });
 
